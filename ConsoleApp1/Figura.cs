f29930e0c33102dcc36f6c58b15ccbb48d693724
KirillShet <146167926+KirillShet@users.noreply.github.com>
2023-10-17 13:37:36 +0300

    Add files via upload

---
+++ ConsoleApp1/Figura.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class square
+    {
+        public float a;
+        public float b;
+        public float S;
+        public float P;
+        public void info()
+        {
+            Console.WriteLine("Введите сторону фигуры.");
+            a = Convert.ToInt32(Console.ReadLine());
+        }
+        public float perimeter()
+        {
+            P = a * 4;
+            Console.WriteLine("Периметр фигуры: "+P);
+            return P;
+        }
+        public float Square()
+        {
+            S = (float)Math.Pow(a, 2);
+            Console.WriteLine("Площадь фигуры: "+S);
+            return S;
+        }
+    }
+    internal class rectangle
+    {
+        public float a;
+        public float b;
+        public float S;
+        public float P;
+        public void info()
+        {
+            Console.WriteLine("Введите сторону фигуры.");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+        }
+        public float perimeter()
+        {
+            P = (a + b) * 2;
+            Console.WriteLine("Периметр фигуры: "+P);
+            return P;
+        }
+        public float square()
+        {
+            S = a*b;
+            Console.WriteLine("Площадь фигуры: " + S);
+            return S;
+        }
+    }
+    internal class triangle
+    {
+        public float a;
+        public float b;
+        public float c;
+        public float S;
+        public float P;
+        public float p;
+        public bool pov;
+        public void info()
+        {
+            Console.WriteLine("Введите сторону фигуры.");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            c = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public bool prov()
+        {
+            if ((a + b) > c && (a + c) > b && (b + c) > a)
+            {
+                pov = true;
+                return pov;
+            }
+            else
+            {
+                pov = false;
+                return pov;
+            }
+        }
+        public float perimeter()
+        {
+            P = a + b + c;
+            Console.WriteLine("Периметр фигуры: " + P);
+            return P;
+        }
+        public float half_meter()
+        {
+            p = (a + b + c) / 2;
+            return p;
+        }
+        public float square()
+        {
+            S = (float)Math.Sqrt(half_meter()*(half_meter()-a)* (half_meter() - b)* (half_meter() - c));
+            Console.WriteLine("Площадь фигуры: " + S);
+            return S;
+        }
+    }
+    internal class circle
+    {
+        public float r;
+        public float S;
+        public float P;
+        public void info()
+        {
+            r = Convert.ToInt32(Console.ReadLine());
+        }
+        public float square()
+        {
+            S = (float)Math.PI * (float)Math.Pow(r, 2);
+            Console.WriteLine("Площадь фигуры: " + S);
+            return S;
+        }
+        public float perimeter()
+        {
+            P = 2 * (float)Math.PI * r;
+            Console.WriteLine("Периметр фигуры: " + P);
+            return P;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                int var = 0;
+                triangle tri = new triangle();
+                circle cir = new circle();
+                rectangle rec = new rectangle();
+                square squ = new square();
+                Console.WriteLine("1 - квадрат, 2 - прямоугольник, 3 - круг, 4 - тругольник.");
+                var = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+                if (var == 1)
+                {
+                    Console.WriteLine("Квадрат");
+                    squ = new square();
+                    squ.info();
+                    squ.perimeter();
+                    squ.Square();
+                }
+                else if (var == 2)
+                {
+                    Console.WriteLine("Прямоугольник");
+                    rec = new rectangle();
+                    rec.info();
+                    rec.perimeter();
+                    rec.square();
+                }
+                else if (var == 3)
+                {
+                    Console.WriteLine("Круг");
+                    cir = new circle();
+                    cir.info();
+                    cir.perimeter();
+                    cir.square();
+                }
+                else if (var == 4)
+                {
+                    Console.WriteLine("Треугольник");
+                    tri = new triangle();
+                    tri.info();
+                    tri.prov();
+                    if (tri.prov() == false)
+                    {
+                        Console.WriteLine("Такого треугольника не существует.");
+                    }
+                    else
+                    {
+                        tri.half_meter();
+                        tri.perimeter();
+                        tri.square();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Вы ввели не то число.");
+                }
+            }
+        }
+    }
+}
